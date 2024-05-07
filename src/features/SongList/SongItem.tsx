caf176612230602d77b2467fb6d1f686f64effa3
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 02:07:44 +0300

    refactor SongItem component to truncate title and body text

--- src/features/SongList/SongItem.tsx
+++ src/features/SongList/SongItem.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Song } from "../../redux/types";
 
-const SongItem: React.FC = () => {
-  return <div>SongItem</div>;
+interface Props {
+  song: Song;
+}
+
+const SongItem: React.FC<Props> = ({ song }) => {
+  const maxTitleLength = 20;
+  const maxBodyLength = 100;
+  const truncatedTitle =
+    song.title.length > maxTitleLength
+      ? song.title.substring(0, maxTitleLength) + "..."
+      : song.title;
+  const truncatedBody =
+    song.body.length > maxBodyLength
+      ? song.body.substring(0, maxBodyLength) + "..."
+      : song.body;
+  return (
+    <div>
+      <h3>{truncatedTitle}</h3>
+      <p>{truncatedBody}</p>
+    </div>
+  );
 };
 
 export default SongItem;
