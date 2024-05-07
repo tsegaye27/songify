99be164a8facf7b21523301ad0bf2e1ce0965dde
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 00:04:07 +0300

    feat: Refactor routing in App.tsx to use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import SongList from "./features/SongList/SongList";
+import AddSong from "./features/AddSong/AddSong";
+import EditSong from "./features/EditSong/EditSong";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/songs",
+        element: <SongList />,
+      },
+      {
+        path: "/songs/add",
+        element: <AddSong />,
+      },
+      {
+        path: "/songs/edit/:id",
+        element: <EditSong />,
+      },
+    ],
+  },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
