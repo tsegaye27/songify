f85b2cb60027581c7c91ec7c19a91cf000c5ac5d
tsegaye27 <ttalegn29@gmail.com>
2024-05-07 05:13:40 +0300

    removing some files

--- src/store/songs/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-import { Song } from "./types";
-
-export const fetchSongs = createAction("songs/fetch");
-export const fetchSongsSuccess = createAction<Song[]>("songs/fetchSuccess");
-export const addSong = createAction<Song>("songs/add");
-export const updateSong = createAction<Song>("songs/update");
-export const deleteSong = createAction<number>("songs/delete");
