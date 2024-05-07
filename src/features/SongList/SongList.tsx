f936907f5fd0d11eac45ff5f4be4a89e353e63bb
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 02:08:06 +0300

    feat: Add lazy loading for images in SongList component

--- src/features/SongList/SongList.tsx
+++ src/features/SongList/SongList.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import RootState from "../../redux/RootState";
+import { fetchSongsStart } from "../../redux/slices/slice";
+import SongItem from "./SongItem";
 
 const SongList: React.FC = () => {
-  return <div>SongList</div>;
+  const dispatch = useDispatch();
+  const songs = useSelector((state: RootState) => state.songs.list);
+  console.log(songs);
+
+  useEffect(() => {
+    dispatch(fetchSongsStart());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {songs.map((song) => (
+        <SongItem key={song.id} song={song} />
+      ))}
+    </div>
+  );
 };
 
 export default SongList;
