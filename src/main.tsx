bdad10a68e45613cc1d78f47bcbbd25dcf57caa5
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 02:07:55 +0300

    refactor: Remove commented out code in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
@@ -7,8 +7,8 @@ import store from "./redux/store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <App />
+    {/* </React.StrictMode> */}
   </Provider>
 );
