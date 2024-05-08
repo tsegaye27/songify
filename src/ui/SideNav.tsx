3ff588f37ee488e85881c37fd819dd1b8ed2dde9
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 21:10:25 +0300

    refactor: Update SideNav component to use styled components for layout and add logo

--- src/ui/SideNav.tsx
+++ src/ui/SideNav.tsx
@@ -1,17 +1,64 @@
+import styled from "@emotion/styled";
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import logo from "../../public/logo.png";
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100svh;
+  margin-top: 3rem;
+`;
+
+const Logo = styled.img`
+  width: 8rem;
+  height: 8rem;
+  margin: auto;
+`;
+
+const StyledSideNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #312f2f;
+  width: 20rem;
+  height: 100svh;
+  margin-right: 1rem;
+`;
+const LogoContainer = styled.div`
+  height: 30%;
+  padding: 1rem 1rem;
+  background-color: #cff0d1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const H1 = styled.h1`
+  margin: auto;
+  color: black;
+  font-family: monospace;
+`;
 
 const SideNav: React.FC = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="/songs/add">Add Song</Link>
-      <br />
-      <Link to="/songs/edit/:id">Edit Song</Link>
-      <br />
-      <Link to="/songs">Songs</Link>
-    </div>
+    <StyledSideNav>
+      <LogoContainer>
+        <Logo src={logo} />
+        <H1>Songify</H1>
+      </LogoContainer>
+      <Nav>
+        <NavLink className="nav-links" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-links" to="/songs">
+          Songs
+        </NavLink>
+        <NavLink className="nav-links" to="/playlists">
+          Playlists
+        </NavLink>
+      </Nav>
+    </StyledSideNav>
   );
 };
 
