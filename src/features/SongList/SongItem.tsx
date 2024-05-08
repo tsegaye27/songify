44d42b31d40f56af54c3824fd8b0d10ea0144801
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 21:08:29 +0300

    refactor: Update SongItem component to include EditSong functionality

--- src/features/SongList/SongItem.tsx
+++ src/features/SongList/SongItem.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Song } from "../../redux/types";
+import EditSong from "./EditSong/EditSong";
+import styled from "@emotion/styled";
 
+const StyledSongItem = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  h3 {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+  p {
+    font-size: 1rem;
+  }
+  button {
+    margin-top: 10px;
+  }
+`;
 interface Props {
   song: Song;
 }
 
 const SongItem: React.FC<Props> = ({ song }) => {
+  const [selectedId, setSelectedId] = useState<string | null>(null);
   const maxTitleLength = 20;
-  const maxBodyLength = 100;
+  const maxBodyLength = 20;
   const truncatedTitle =
     song.title.length > maxTitleLength
       ? song.title.substring(0, maxTitleLength) + "..."
@@ -16,11 +37,22 @@ const SongItem: React.FC<Props> = ({ song }) => {
     song.body.length > maxBodyLength
       ? song.body.substring(0, maxBodyLength) + "..."
       : song.body;
+
+  function handleEdit(songId: string | null) {
+    setTimeout(() => {
+      setSelectedId(songId);
+    }, 1000);
+  }
+
   return (
-    <div>
-      <h3>{truncatedTitle}</h3>
-      <p>{truncatedBody}</p>
-    </div>
+    <>
+      <StyledSongItem>
+        <h3>{truncatedTitle}</h3>
+        <p>{truncatedBody}</p>
+        <button onClick={() => handleEdit(song.id)}>Edit Song</button>
+      </StyledSongItem>
+      {selectedId && <EditSong onUpdate={handleEdit} songId={selectedId} />}
+    </>
   );
 };
 
