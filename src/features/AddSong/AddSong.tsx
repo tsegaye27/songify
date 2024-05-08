d15d4066614509a76ab42cb6de69159337f72586
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 18:55:25 +0300

    removing some files

--- src/features/AddSong/AddSong.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddSong: React.FC = () => {
-  return <div>AddSong</div>;
-};
-
-export default AddSong;
--- src/features/EditSong/EditSong.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditSong: React.FC = () => {
-  return <div>EditSong</div>;
-};
-
-export default EditSong;
