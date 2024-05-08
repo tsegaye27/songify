b7fde1b87202ba73aa7cf332bf0a623df5990592
tsegaye27 <ttalegn29@gmail.com>
2024-05-08 21:10:15 +0300

    refactor: Update AppLayout component to use styled components for layout

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import SideNav from "./SideNav";
 import { Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const StyledAppLayout = styled.div`
+  font-weight: bold;
+  display: flex;
+`;
+
+const MiniContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const AppLayout: React.FC = () => {
   return (
-    <div>
-      <header>Header</header>
+    <StyledAppLayout>
       <SideNav />
-      <main>
-        <Outlet />
-      </main>
-      <footer>Footer</footer>
-    </div>
+      <MiniContainer>
+        <header>Header</header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>Footer</footer>
+      </MiniContainer>
+    </StyledAppLayout>
   );
 };
 
