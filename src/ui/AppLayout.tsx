2bbe569cf2c52343294f9614768f309918b859e3
tsegaye27 <ttalegn29@gmail.com>
2024-05-09 19:02:03 +0300

    refactor: Update AppLayout component to include Header and Footer components

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import SideNav from "./SideNav";
 import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
+import Header from "./Header";
 
 const StyledAppLayout = styled.div`
-  font-weight: bold;
   display: flex;
+  height: 100vh;
+`;
+
+const StyledMain = styled.main`
+  padding: 2rem;
+  flex: 1;
+  overflow-y: auto;
 `;
 
 const MiniContainer = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
+  background-color: #494949;
+`;
+
+const Footer = styled.footer`
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
 `;
 
 const AppLayout: React.FC = () => {
@@ -18,11 +34,11 @@ const AppLayout: React.FC = () => {
     <StyledAppLayout>
       <SideNav />
       <MiniContainer>
-        <header>Header</header>
-        <main>
+        <Header />
+        <StyledMain>
           <Outlet />
-        </main>
-        <footer>Footer</footer>
+        </StyledMain>
+        <Footer>Songify | All Rights Reserved</Footer>
       </MiniContainer>
     </StyledAppLayout>
   );
