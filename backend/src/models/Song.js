315c39b4ecdb64aa5ba3f07249999a062339564f
tsegaye27 <ttalegn29@gmail.com>
2024-05-09 23:42:22 +0300

    refactor: Remove commented code and unnecessary fields from Song model

--- backend/src/models/Song.js
+++ backend/src/models/Song.js
@@ -4,21 +4,11 @@ const songSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    // minLength: 1,
-    // maxLength: 25,
   },
   artist: {
     type: String,
     required: true,
-    // minLength: 1,
-    // maxLength: 25,
   },
-  // genre: {
-  //   type: String,
-  //   required: true,
-  //   minLength: 1,
-  //   maxLength: 25,
-  // },
 });
 
 const Song = mongoose.model("Song", songSchema);
