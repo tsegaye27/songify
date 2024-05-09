bd939d791e0fb15f58e456a1a2e9316319df41fa
tsegaye27 <ttalegn29@gmail.com>
2024-05-10 01:42:06 +0300

    refactor: Simplify getAllSongs response in songsController

--- backend/src/controllers/songsController.js
+++ backend/src/controllers/songsController.js
@@ -3,11 +3,7 @@ const Song = require("../models/Song");
 exports.getAllSongs = async (req, res) => {
   try {
     const songs = await Song.find();
-    res.status(200).json({
-      status: "success",
-      results: songs.length,
-      data: songs,
-    });
+    res.status(200).json(songs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -40,5 +36,37 @@ exports.addSong = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+};
+
+exports.updateSong = async (req, res) => {
+  try {
+    const song = await Song.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!song) {
+      return res.status(404).json({ message: "Song not found" });
+    }
+    res.status(200).json({
+      status: "success",
+      data: song,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteSong = async (req, res) => {
+  try {
+    const deletedSong = await Song.findByIdAndDelete(req.params.id);
+    if (deletedSong) {
+      res.status(204).json({
+        message: "Song Deleted",
+      });
+    } else {
+      res.status(404).json({ message: "Song not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
