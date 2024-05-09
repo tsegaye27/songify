9b77a93325afa33c5403bb8495cb1ddb4daf0522
tsegaye27 <ttalegn29@gmail.com>
2024-05-09 19:59:34 +0300

    refactor: Update EditSong component with styled components for layout and clear form inputs after submitting

--- src/features/SongList/EditSong/EditSong.tsx
+++ src/features/SongList/EditSong/EditSong.tsx
@@ -1,11 +1,46 @@
 import React, { useState } from "react";
-import { updateSongStart } from "../../../redux/slices/slice";
 import { useDispatch } from "react-redux";
+import { updateSongStart } from "../../../redux/slices/slice";
+import styled from "@emotion/styled";
 
 interface EditSongProps {
   songId: string;
-  onUpdate: (songId: string | null) => void;
+  onUpdate: (id: string | null) => void;
 }
+
+const EditSongContainer = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.2);
+`;
+
+const Title = styled.h3`
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  margin-bottom: 10px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 const EditSong: React.FC<EditSongProps> = ({ songId, onUpdate }) => {
   const [title, setTitle] = useState("");
@@ -18,39 +53,31 @@ const EditSong: React.FC<EditSongProps> = ({ songId, onUpdate }) => {
     dispatch(updateSongStart({ id: songId, title, body }));
     setTitle("");
     setBody("");
+    onUpdate(null);
   }
 
   return (
-    <div>
-      <h3>Edit Song</h3>
-      <form onSubmit={handleSubmit}>
-        <input
+    <EditSongContainer>
+      <Title>Edit Song</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
           type="text"
           placeholder="Enter the Title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           required
         />
-        <br />
-        <input
+        <Input
           type="text"
           placeholder="Enter the Artist"
           value={body}
           onChange={(e) => setBody(e.target.value)}
           required
         />
-        <br />
-        <button
-          onClick={() => {
-            if (!title || !body) return;
-            onUpdate(null);
-          }}
-          type="submit"
-        >
-          Update Song
-        </button>
-      </form>
-    </div>
+        <Button type="submit">Update Song</Button>
+        <Button onClick={() => onUpdate(null)}>Cancel</Button>
+      </Form>
+    </EditSongContainer>
   );
 };
 
