e1b6690308b4ff9748c1f124c540ecc0367fc316
tsegaye27 <ttalegn29@gmail.com>
2024-05-10 13:53:44 +0300

    resolving the CORS issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 require("dotenv").config({ path: "config.env" });
 const songsRouter = require("./routes/songsRouter");
 
@@ -12,12 +13,13 @@ const db = process.env.DB;
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use("/songs", songsRouter);
 
