b981ff691de50be2f0b007873132ec8bdf8a34ce
tsegaye27 <ttalegn29@gmail.com>
2024-05-10 06:34:56 +0300

    refactor: Update Header component background styling

--- frontend/src/ui/Header.tsx
+++ frontend/src/ui/Header.tsx
@@ -1,14 +1,35 @@
 import styled from "@emotion/styled";
-import { BiSearch } from "react-icons/bi"; // Import the BiSearch icon
+import { BiSearch } from "react-icons/bi";
 
 const StyledHeader = styled.header`
-  background-color: #181818;
+  /* background-color: #181818; */
+  background-image: url("../../public/banner.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: rgba(0, 0, 0, 0.1);
   color: #ffffff;
   padding: 3rem 4rem 1rem 4rem;
   display: flex;
+  height: 10%;
   align-items: center;
   justify-content: center;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(10px);
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: url("../../public/banner.jpg");
+    background-position: center;
+    background-size: cover;
+    filter: blur(5px);
+    z-index: -1;
+  }
 `;
 
 const SearchContainer = styled.div`
@@ -17,7 +38,9 @@ const SearchContainer = styled.div`
   background-color: #333333;
   border-radius: 24px;
   padding: 8px 16px;
-
+  width: 20rem;
+  justify-content: space-between;
+  /* z-index: 1000; */
   border: 1px solid #00e1ff;
   /* border: 1px solid #00e1ff; */
 `;
