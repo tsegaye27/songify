109e090f02ed85aa32c978c9ecc9274f89666e4c
tsegaye27 <ttalegn29@gmail.com>
2024-05-10 17:55:01 +0300

    updating the useEffect dependencies

--- frontend/src/features/SongList/SongList.tsx
+++ frontend/src/features/SongList/SongList.tsx
@@ -4,8 +4,6 @@ import RootState from "../../redux/RootState";
 import { fetchSongsStart } from "../../redux/slices/slice";
 import SongItem from "./SongItem";
 import styled from "@emotion/styled";
-import { BiEdit, BiHeart, BiPlus, BiTrash } from "react-icons/bi";
-import { BsMusicNote } from "react-icons/bs";
 
 const StyledSongList = styled.div`
   display: grid;
@@ -17,66 +15,66 @@ const StyledSongList = styled.div`
   margin-top: 1rem;
 `;
 
-const StyledSongItem = styled.div`
-  background-color: #222;
-  border-radius: 10px;
-  box-shadow: 0px 2px 4px rgba(255, 255, 255, 0.1);
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  transition: transform 0.3s ease;
+// const StyledSongItem = styled.div`
+//   background-color: #222;
+//   border-radius: 10px;
+//   box-shadow: 0px 2px 4px rgba(255, 255, 255, 0.1);
+//   padding: 20px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   transition: transform 0.3s ease;
 
-  &:hover {
-    transform: translateY(-5px);
-  }
-`;
+//   &:hover {
+//     transform: translateY(-5px);
+//   }
+// `;
 
-const SongLogo = styled(BsMusicNote)`
-  font-size: 32px;
-  color: #ccc;
-  margin-bottom: 20px;
-`;
+// const SongLogo = styled(BsMusicNote)`
+//   font-size: 32px;
+//   color: #ccc;
+//   margin-bottom: 20px;
+// `;
 
 const Title = styled.h3`
   font-size: 20px;
   color: #ccc;
   margin: 0;
+  width: 100%;
   text-align: center;
 `;
 
-const Body = styled.p`
-  font-size: 16px;
-  color: #aaa;
-  margin: 10px 0;
-  text-align: center;
-`;
+// const Body = styled.p`
+//   font-size: 16px;
+//   color: #aaa;
+//   margin: 10px 0;
+//   text-align: center;
+// `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-top: 20px;
-`;
+// const ButtonContainer = styled.div`
+//   display: flex;
+//   gap: 10px;
+//   margin-top: 20px;
+// `;
 
-const Icon = styled.span`
-  font-size: 20px;
-  cursor: pointer;
-  background-color: #222;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  transition: box-shadow 0.3s ease;
+// const Icon = styled.span`
+//   font-size: 20px;
+//   cursor: pointer;
+//   background-color: #222;
+//   color: #fff;
+//   border: none;
+//   padding: 10px 20px;
+//   border-radius: 5px;
+//   transition: box-shadow 0.3s ease;
 
-  &:hover {
-    box-shadow: 1px 1px 5px #fea22a;
-  }
-`;
+//   &:hover {
+//     box-shadow: 1px 1px 5px #fea22a;
+//   }
+// `;
 
 const SongList: React.FC = () => {
   const dispatch = useDispatch();
   const songs = useSelector((state: RootState) => state.songs.list);
-  console.log(songs);
 
   useEffect(() => {
     dispatch(fetchSongsStart());
@@ -84,33 +82,15 @@ const SongList: React.FC = () => {
 
   return (
     <div>
-      <StyledSongList>
-        {songs.length === 0 ? (
-          <StyledSongItem>
-            <SongLogo className="song-logo" />
-            <Title>Sample Title</Title>
-            <Body>Sample Body</Body>
-            <ButtonContainer>
-              <Icon>
-                <BiEdit />
-              </Icon>
-              <Icon>
-                <BiTrash />
-              </Icon>
-              <Icon>
-                <BiPlus />
-              </Icon>
-              <Icon>
-                <BiHeart />
-              </Icon>
-            </ButtonContainer>
-          </StyledSongItem>
-        ) : (
-          songs.map((song) => (
+      {songs.length === 0 ? (
+        <Title>No Songs Yet ...</Title>
+      ) : (
+        <StyledSongList>
+          {songs.map((song) => (
             <SongItem key={song._id} song_Id={song._id} song={song} />
-          ))
-        )}
-      </StyledSongList>
+          ))}
+        </StyledSongList>
+      )}
     </div>
   );
 };
