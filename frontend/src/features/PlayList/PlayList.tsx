c49217c41e2cc68c9c7dac425fe3438e5f01cbc7
tsegaye27 <ttalegn29@gmail.com>
2024-05-10 21:35:03 +0300

    feat: Set page title to "Playlist" in PlayList component

--- frontend/src/features/PlayList/PlayList.tsx
+++ frontend/src/features/PlayList/PlayList.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const PlayList: React.FC = () => {
+  useEffect(() => {
+    document.title = "Playlist";
+  }, []);
   return <div>PlayList</div>;
 };
 
