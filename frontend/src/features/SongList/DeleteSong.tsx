2b17ba56501278e97378ea24171fecd20b6534f8
tsegaye27 <ttalegn29@gmail.com>
2024-05-11 15:37:37 +0300

    styling DeleteSong component

--- frontend/src/features/SongList/DeleteSong.tsx
+++ frontend/src/features/SongList/DeleteSong.tsx
@@ -3,33 +3,49 @@ import { useDispatch, useSelector } from "react-redux";
 import RootState from "../../redux/RootState";
 import { deleteSongStart } from "../../redux/slices/slice";
 import styled from "@emotion/styled";
+import { BiCheck, BiX } from "react-icons/bi";
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #222;
+  border: 1px solid #007bff;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-around;
+  height: 100%;
+  align-items: flex-end;
 `;
 
-const Button = styled.button`
-  padding: 15px;
-  background-color: #222;
-  color: white;
-  border: 1px solid white;
-  border-radius: 4px;
-  cursor: pointer;
+const DeleteButton = styled(BiCheck)`
+  color: #00ff00;
+  font-size: 20px;
+  &:hover {
+  }
+`;
+const CancelButton = styled(BiX)`
+  color: red;
+  font-size: 20px;
 `;
 
 const StyledDeleteSong = styled.div`
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  height: 15rem;
+  padding: 20px;
   width: 20rem;
+  height: 10rem;
 `;
 
-const H2 = styled.h2`
+const Title = styled.h3`
+  color: white;
+  margin-bottom: 20px;
   text-align: center;
 `;
-
 interface DeleteSongProps {
   onDelete: () => void;
   song_Id: string;
@@ -46,10 +62,14 @@ const DeleteSong: React.FC<DeleteSongProps> = ({ onDelete, song_Id }) => {
   }
   return (
     <StyledDeleteSong>
-      <H2>{`Delete ${filteredSong[0].title} ?`}</H2>
+      <Title>{`Delete ${filteredSong[0].title}?`}</Title>
       <ButtonContainer>
-        <Button onClick={() => handleDelete(filteredSong[0]._id)}>Yes</Button>
-        <Button onClick={onDelete}>No</Button>
+        <Button onClick={() => handleDelete(filteredSong[0]._id)}>
+          <DeleteButton />
+        </Button>
+        <Button onClick={onDelete}>
+          <CancelButton />
+        </Button>
       </ButtonContainer>
     </StyledDeleteSong>
   );
