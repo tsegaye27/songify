80df0d58a9f441d29cf12cfb6692bc2e8e843258
tsegaye27 <ttalegn29@gmail.com>
2024-05-11 20:23:54 +0300

    including the states for the playlist and favorite on to RootState

--- frontend/src/redux/RootState.ts
+++ frontend/src/redux/RootState.ts
@@ -1,7 +1,11 @@
 import { SongState } from "./slices/slice";
+import { FavoriteSongState } from "./slices/favoriteSlice";
+import { PlaylistState } from "./slices/playlistSlice";
 
 interface RootState {
   songs: SongState;
+  favorites: FavoriteSongState;
+  playlist: PlaylistState;
 }
 
 export default RootState;
