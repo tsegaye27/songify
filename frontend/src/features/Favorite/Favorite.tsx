70c98326adf3087e857379b37e0466829d0e5109
tsegaye27 <ttalegn29@gmail.com>
2024-05-12 04:56:56 +0300

    feat: Add favorite songs grid to Favorite component

--- frontend/src/features/Favorite/Favorite.tsx
+++ frontend/src/features/Favorite/Favorite.tsx
@@ -1,10 +1,44 @@
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import RootState from "../../redux/RootState";
+import SongItem from "../SongList/SongItem";
+import styled from "@emotion/styled";
+const StyledFavorite = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  gap: 1rem;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+  color: white;
+`;
 
 const Favorite: React.FC = () => {
+  const favorites = useSelector((state: RootState) => state.favorites.favList);
+
   useEffect(() => {
     document.title = "Favorites";
   }, []);
-  return <div>Favorite</div>;
+
+  return (
+    <>
+      {" "}
+      {favorites.length === 0 ? (
+        <H2>No Favorite Songs yet...</H2>
+      ) : (
+        <StyledFavorite>
+          {favorites.map((song) => (
+            <SongItem key={song._id} song={song} />
+          ))}
+        </StyledFavorite>
+      )}{" "}
+    </>
+  );
 };
 
 export default Favorite;
