3eef69a15a711c0b446b9a34e113ba9b0cf5d47d
tsegaye27 <ttalegn29@gmail.com>
2024-05-14 22:45:41 +0300

    feat: Add styled Playlist component

--- frontend/src/features/Playlist/Playlist.tsx
+++ frontend/src/features/Playlist/Playlist.tsx
@@ -4,18 +4,28 @@ import {
   Title,
 } from "../SongList/SongList";
 import { BiPlus } from "react-icons/bi";
+import styled from "@emotion/styled";
+
+const StyledPlaylist = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+`;
 
 const Playlist: React.FC = () => {
   useEffect(() => {
     document.title = "Playlist";
   }, []);
   return (
-    <Styled>
+    <StyledPlaylist>
       <Title>No Playlist yet, Start by creating a new one</Title>
       <AddPlaylistButton>
         <BiPlus />
       </AddPlaylistButton>
-    </Styled>
+    </StyledPlaylist>
   );
 };
 
