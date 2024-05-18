d8b284f07fc71a9f33a16e9c794cf4149d360826
tsegaye27 <ttalegn29@gmail.com>
2024-05-18 20:52:53 +0300

    refactoring

--- frontend/src/features/Playlist/Playlist.tsx
+++ frontend/src/features/Playlist/Playlist.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AddSongButton as AddPlaylistButton,
   Title,
 } from "../SongList/SongList";
 import { BiPlus } from "react-icons/bi";
 import styled from "@emotion/styled";
+import Modal from "../../ui/Modal";
+import AddPlaylist from "./AddPlaylist";
+import { useSelector } from "react-redux";
+import RootState from "../../redux/RootState";
+import PlaylistItem from "./PlaylistItem";
 
 const StyledPlaylist = styled.div`
   display: flex;
@@ -16,16 +21,37 @@ const StyledPlaylist = styled.div`
 `;
 
 const Playlist: React.FC = () => {
+  const [isAddPlaylistClicked, setIsAddPlaylistClicked] =
+    useState<boolean>(false);
+  const list = useSelector((state: RootState) => state.playlist.listOfPlaylist);
   useEffect(() => {
     document.title = "Playlist";
   }, []);
+  function handleAddPlaylist() {
+    setIsAddPlaylistClicked((b) => !b);
+  }
   return (
-    <StyledPlaylist>
-      <Title>No Playlist yet, Start by creating a new one</Title>
-      <AddPlaylistButton onClick={() => console.log("add playlist")}>
-        <BiPlus />
-      </AddPlaylistButton>
-    </StyledPlaylist>
+    <div>
+      {list.length === 0 ? (
+        <StyledPlaylist>
+          <Title>No Playlist yet, Start by creating a new one</Title>
+          <AddPlaylistButton onClick={handleAddPlaylist}>
+            <BiPlus />
+          </AddPlaylistButton>
+        </StyledPlaylist>
+      ) : (
+        <>
+          {list.map((item) => (
+            <PlaylistItem key={item.name} item={item} />
+          ))}
+        </>
+      )}
+      {isAddPlaylistClicked && (
+        <Modal>
+          <AddPlaylist />
+        </Modal>
+      )}
+    </div>
   );
 };
 
