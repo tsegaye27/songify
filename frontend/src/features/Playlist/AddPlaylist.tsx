bb3fb673dca3a91eb1101691661822407d0465c1
tsegaye27 <ttalegn29@gmail.com>
2024-05-18 20:52:28 +0300

    feat: Add form for creating a new playlist

--- frontend/src/features/Playlist/AddPlaylist.tsx
+++ frontend/src/features/Playlist/AddPlaylist.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Title } from "../SongList/SongList";
+import styled from "@emotion/styled";
+import { Input } from "../SongList/AddSong/AddSong";
+import { Form } from "react-router-dom";
+
+const StyledAddPlaylist = styled.div`
+  width: 100%;
+`;
 
 const AddPlaylist: React.FC = () => {
-  return <div>AddPlaylist</div>;
+  return (
+    <StyledAddPlaylist>
+      <Form>
+        <Title>Enter Playlist name</Title>
+        <Input type="text" placeholder="enter the name here..." />
+      </Form>
+    </StyledAddPlaylist>
+  );
 };
 
 export default AddPlaylist;
