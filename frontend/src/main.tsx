bd1798208d704ea6feeeb5e59aeba75ff58ddfe8
tsegaye27 <ttalegn29@gmail.com>
2024-05-20 01:00:58 +0300

    Refactor main.tsx to remove unused imports and comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.ts";
-// import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    {/* <React.StrictMode> */}
     <App />
-    {/* </React.StrictMode> */}
   </Provider>
 );
