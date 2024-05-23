fbbfbcd62f2f86fdba2204c001462cfc71de956c
tsegaye27 <ttalegn29@gmail.com>
2024-05-23 15:03:16 +0300

    feat: Create searchSlice for managing search query state

---
+++ frontend/src/redux/slices/searchSlice.ts
@@ -0,0 +1,23 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface SearchState {
+  query: string;
+}
+
+const initialState: SearchState = {
+  query: "",
+};
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setSearchQuery(state, action: PayloadAction<string>) {
+      state.query = action.payload;
+    },
+  },
+});
+
+export const { setSearchQuery } = searchSlice.actions;
+
+export default searchSlice.reducer;
