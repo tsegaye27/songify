1cc5a64905dfa254df430e05a3c8d74f7b8a103a
tsegaye27 <ttalegn29@gmail.com>
2024-05-24 16:41:51 +0300

    feat: Add Playlist model

---
+++ backend/src/models/Playlist.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    songs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
+module.exports = Playlist;
