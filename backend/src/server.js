d5f95e2cd7875b918ecadeba13f1648073617564
tsegaye27 <ttalegn29@gmail.com>
2024-05-28 12:05:56 +0300

    setting up for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,13 +15,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
 
+app.use("/ping", (req, res) => {
+  res.send("pong");
+});
 app.use("/songs", songsRouter);
 app.use("/playlists", playlistsRouter);
 
