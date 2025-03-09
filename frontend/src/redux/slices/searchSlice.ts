357578c09f743be457847fca1cb244ad9d0889d6
tsegaye27 <ttalegn29@gmail.com>
2025-03-09 18:57:33 +0300

    feat(search): add selector to retrieve search query from state

--- frontend/src/redux/slices/searchSlice.ts
+++ frontend/src/redux/slices/searchSlice.ts
@@ -1,8 +1,9 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import RootState from "../RootState";
 
-export type SearchState = {
+export interface SearchState {
   query: string;
-};
+}
 
 const initialState: SearchState = {
   query: "",
@@ -12,12 +13,12 @@ const searchSlice = createSlice({
   name: "search",
   initialState,
   reducers: {
-    setSearchQuery(state, action: PayloadAction<string>) {
+    setSearchQuery: (state, action: PayloadAction<string>) => {
       state.query = action.payload;
     },
   },
 });
 
 export const { setSearchQuery } = searchSlice.actions;
-
+export const selectSearchQuery = (state: RootState) => state.search.query;
 export default searchSlice.reducer;
