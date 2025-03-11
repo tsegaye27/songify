1db8e653d82471776c3ebec9b052946669306300
tsegaye27 <ttalegn29@gmail.com>
2025-03-11 15:19:08 +0300

    style(applayout): enhance app layout aesthetics and footer

--- frontend/src/ui/AppLayout.tsx
+++ frontend/src/ui/AppLayout.tsx
@@ -18,16 +18,23 @@ const StyledMain = styled.main`
   scrollbar-color: var(--app-layout-color-hover) transparent;
 
   &::-webkit-scrollbar {
-    background-color: var(--app-layout-color-hover);
+    width: 8px; /* Customize scrollbar width */
+    background-color: var(--second-background-color);
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--accent-color);
     border-radius: 10px;
   }
 `;
 
 const Footer = styled.footer`
   background-color: var(--second-background-color);
-  color: var(--tertiary-color);
+  color: var(--text-color);
   text-align: center;
   padding: 10px;
+  box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.2); /* Subtle shadow for depth */
 `;
 
 const MemoizedSideNav = memo(SideNav);
@@ -49,7 +56,7 @@ const AppLayout: React.FC = () => {
         <StyledMain>
           <Outlet />
         </StyledMain>
-        <Footer>Songify | All Rights Reserved</Footer>
+        <Footer>Songify 2025 | &copy; All Rights Reserved</Footer>
       </div>
     </StyledAppLayout>
   );
