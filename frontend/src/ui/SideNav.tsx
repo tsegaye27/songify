fe51b4d1e6f0d621fab451cc173bcda498cc916b
tsegaye27 <ttalegn29@gmail.com>
2025-03-11 15:20:35 +0300

    feat(SideNav): Implement new SideNav design with logo and updated links

--- frontend/src/ui/SideNav.tsx
+++ frontend/src/ui/SideNav.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import styled from "@emotion/styled";
-import { BiHome, BiMusic, BiCog } from "react-icons/bi";
+import { BiMusic, BiHeart } from "react-icons/bi";
+import { BsCollectionPlay } from "react-icons/bs";
+import logo from "../../public/logo.png";
 
 const StyledNav = styled.nav`
   width: 240px;
-  background-color: var(--sidebar-bg);
+  background-color: var(--second-background-color);
   padding: 20px;
   display: flex;
   flex-direction: column;
+  box-shadow: 2px 0 10px rgba(0, 0, 0, 0.5);
 `;
 
 const StyledNavLink = styled(NavLink)`
@@ -18,14 +21,18 @@ const StyledNavLink = styled(NavLink)`
   color: var(--text-color);
   text-decoration: none;
   border-radius: 6px;
-  transition: background 0.3s ease;
+  transition:
+    background 0.3s ease,
+    color 0.3s ease;
 
   &:hover {
-    background-color: var(--hover-bg);
+    background-color: #333;
+    color: var(--text-color);
   }
 
   &.active {
-    background-color: var(--active-bg);
+    background-color: #222;
+    color: var(--text-color);
     font-weight: bold;
   }
 
@@ -34,10 +41,38 @@ const StyledNavLink = styled(NavLink)`
   }
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 15rem;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid var(--tertiary-color);
+  margin-bottom: 20px;
+`;
+
+const Logo = styled.img`
+  width: 5rem;
+  border-radius: 50%; /* Circular logo */
+`;
+
+const H1 = styled.h1`
+  color: var(--text-color);
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 const links = [
-  { to: "/", icon: <BiHome />, text: "Home" },
   { to: "/songs", icon: <BiMusic />, text: "Songs" },
-  { to: "/settings", icon: <BiCog />, text: "Settings" },
+  { to: "/favorites", icon: <BiHeart />, text: "Favorites" },
+  { to: "/playlists", icon: <BsCollectionPlay />, text: "Playlists" },
 ];
 
 const SideNav: React.FC = () => {
@@ -45,6 +80,12 @@ const SideNav: React.FC = () => {
 
   return (
     <StyledNav>
+      <LogoContainer>
+        <ImageContainer>
+          <Logo src={logo} alt="Logo" />
+        </ImageContainer>
+        <H1>Songify</H1>
+      </LogoContainer>
       {links.map(({ to, icon, text }) => (
         <StyledNavLink
           key={to}
