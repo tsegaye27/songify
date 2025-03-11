4b62c4fe54f086d66aef184432eeae60b8f11800
tsegaye27 <ttalegn29@gmail.com>
2025-03-11 15:26:07 +0300

    chore: update dependencies and devDependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-        },
-      },
-      external: ["fs", "path", "os", "assert", "module", "util"],
-    },
-  },
+  plugins: [
+    react(),
+  ],
 });
