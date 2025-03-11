a20bd5c22664cdc98385a5b63b0abdb21a15e78e
tsegaye27 <ttalegn29@gmail.com>
2025-03-11 15:24:09 +0300

    feat: Implement loading state with Loader component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,56 +7,60 @@ import {
 import AppLayout from "./ui/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 import Error from "./ui/Error";
+import Loader from "./ui/Loader";
 
 const SongList = lazy(() => import("./features/SongList/SongList"));
 const Favorite = lazy(() => import("./features/Favorite/Favorite"));
 const Playlist = lazy(() => import("./features/Playlist/Playlist"));
 
-const router = createBrowserRouter([
-  {
-    element: (
-      <>
-        <AppLayout />
-        <GlobalStyles />
-      </>
-    ),
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        errorElement: <Error />,
-        element: <Navigate to="/songs" replace />,
-      },
-      {
-        path: "/songs",
-        element: (
-          <Suspense fallback={<div>Loading Songs...</div>}>
-            <SongList />
-          </Suspense>
-        ),
-        errorElement: <Error />,
-      },
-      {
-        path: "/favorites",
-        element: (
-          <Suspense fallback={<div>Loading Favorites...</div>}>
-            <Favorite />
-          </Suspense>
-        ),
-        errorElement: <Error />,
-      },
-      {
-        path: "/playlists",
-        element: (
-          <Suspense fallback={<div>Loading Playlists...</div>}>
-            <Playlist />
-          </Suspense>
-        ),
-        errorElement: <Error />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: (
+        <>
+          <AppLayout />
+          <GlobalStyles />
+        </>
+      ),
+      errorElement: <Error />,
+      children: [
+        {
+          path: "/",
+          errorElement: <Error />,
+          element: <Navigate to="/songs" replace />,
+        },
+        {
+          path: "/songs",
+          element: (
+            <Suspense fallback={<Loader />}>
+              <SongList />
+            </Suspense>
+          ),
+          errorElement: <Error />,
+        },
+        {
+          path: "/favorites",
+          element: (
+            <Suspense fallback={<Loader />}>
+              <Favorite />
+            </Suspense>
+          ),
+          errorElement: <Error />,
+        },
+        {
+          path: "/playlists",
+          element: (
+            <Suspense fallback={<Loader />}>
+              <Playlist />
+            </Suspense>
+          ),
+          errorElement: <Error />,
+        },
+      ],
+    },
+  ],
+  {},
+);
 
 const App: React.FC = () => {
   return (
