b5047e482096d2b3e7015a73555fa6781e78f144
tsegaye27 <ttalegn29@gmail.com>
2025-03-11 15:19:16 +0300

    style(button): update button style for better contrast

--- frontend/src/ui/Button.tsx
+++ frontend/src/ui/Button.tsx
@@ -1,9 +1,9 @@
-import styled from "@emotion/styled/macro";
+import styled from "@emotion/styled";
 
 export const Button = styled.button`
   padding: 10px 20px;
-  background-color: var(--primary-color);
-  border: 1px solid #007bff;
+  background-color: #333;
+  border: none;
   color: var(--text-color);
   border-radius: 5px;
   cursor: pointer;
