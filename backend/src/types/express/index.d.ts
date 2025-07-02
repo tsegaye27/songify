5160e4f2973e5f1660b79dcae5ad2a9d0080fd68
tsegaye27 <ttalegn29@gmail.com>
2025-07-02 18:42:43 +0300

    feat: Add user and permission to Express Request

---
+++ backend/src/types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { UserInterface } from "@/models/user/types";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: UserInterface;
+      permission?: any;
+    }
+  }
+}
