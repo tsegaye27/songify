490b08b885d9a4295b22e50939f4c877c4228a5b
tsegaye27 <ttalegn29@gmail.com>
2025-07-02 18:41:36 +0300

    feat: Introduce constants module for configuration and fixed values

---
+++ backend/src/utils/constants.ts
@@ -0,0 +1,12 @@
+import config from "config";
+
+//config values
+export const ENVIRONMENT = config.get("environment");
+export const MORGAN_FORMAT: string = config.get("logging.morgan");
+export const LOG_LEVEL: string = config.get("logging.level");
+
+//const numbers
+export const SALT_ROUNDS = 12;
+export const MAX_TOKEN_AGE = 3 * 24 * 60 * 60;
+export const MAX_VERIFICATION_TOKEN_AGE = 60 * 60 * 1000;
+export const MAX_COOKIE_AGE = MAX_TOKEN_AGE * 1000;
