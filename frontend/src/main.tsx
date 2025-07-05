bdfd27c91e63ae878bb8f7ef7dcac2a96d841d00
tsegaye27 <ttalegn29@gmail.com>
2025-07-06 01:02:19 +0300

    Remove Router wrapper from App component for simplified rendering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,11 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.ts";
 import { checkAuthStart } from "./redux/slices/authSlice.ts";
-import { BrowserRouter as Router } from "react-router-dom";
 
 store.dispatch(checkAuthStart());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+    <App />
+  </Provider>
 );
