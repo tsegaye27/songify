b3761ff9915fe8488db4a2780f6b7865b5b29344
tsegaye27 <ttalegn29@gmail.com>
2025-07-05 21:13:05 +0300

    feat: Expose UI components in a single entry point

---
+++ frontend/src/ui/index.ts
@@ -0,0 +1,11 @@
+// Layout
+export { default as AppLayout } from "./Layout/AppLayout";
+export { default as Sidebar } from "./Layout/Sidebar";
+export { default as Header } from "./Layout/Header";
+
+// Common
+export { default as Button } from "./Common/Button";
+export { default as Modal } from "./Common/Modal";
+export { default as Loader } from "./Common/Loader";
+export { default as EmptyState } from "./Common/EmptyState";
+export { default as Pagination } from "./Common/Pagination";
