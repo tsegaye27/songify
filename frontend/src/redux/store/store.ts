32285d5a642fcae5774c8e7c11df77f77ad38007
tsegaye27 <ttalegn29@gmail.com>
2025-07-05 21:15:47 +0300

    feat: Integrate auth, statistics, and playlist features into Redux store

--- frontend/src/redux/store/store.ts
+++ frontend/src/redux/store/store.ts
@@ -1,45 +1,56 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import { all } from "redux-saga/effects";
-import songsReducer from "../slices/slice";
+
+import songReducer from "../slices/songSlice";
+import playlistReducer from "../slices/playlistSlice";
+import authReducer from "../slices/authSlice";
+import statisticsReducer from "../slices/statisticsSlice";
+import favoritesReducer from "../slices/favoritesSlice";
+
 import {
-  watchAddSong,
-  watchDeleteSong,
   watchFetchSongs,
+  watchAddSong,
   watchUpdateSong,
-} from "../sagas/saga";
+  watchDeleteSong,
+} from "../sagas/songSaga";
 import {
-  watchAddPlaylist,
   watchFetchPlaylists,
+  watchAddPlaylist,
   watchUpdatePlaylist,
   watchDeletePlaylist,
   watchAddSongToPlaylist,
   watchRemoveSongFromPlaylist,
 } from "../sagas/playlistSaga";
-import searchReducer from "../slices/searchSlice";
-import favoritesReducer from "../slices/favoriteSlice";
-import playlistsReducer from "../slices/playlistSlice";
+import { watchAuth } from "../sagas/authSaga";
+import { watchFetchStatistics } from "../sagas/statisticsSaga";
 
 function* rootSaga() {
   yield all([
+    watchAuth(),
+
     watchFetchSongs(),
     watchAddSong(),
-    watchDeleteSong(),
     watchUpdateSong(),
+    watchDeleteSong(),
+
     watchFetchPlaylists(),
     watchAddPlaylist(),
     watchUpdatePlaylist(),
     watchDeletePlaylist(),
     watchAddSongToPlaylist(),
     watchRemoveSongFromPlaylist(),
+
+    watchFetchStatistics(),
   ]);
 }
 
 const rootReducer = combineReducers({
-  songs: songsReducer,
+  songs: songReducer,
+  playlists: playlistReducer,
+  auth: authReducer,
+  statistics: statisticsReducer,
   favorites: favoritesReducer,
-  playlists: playlistsReducer,
-  search: searchReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware();
@@ -47,9 +58,17 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(sagaMiddleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }).concat(sagaMiddleware),
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
