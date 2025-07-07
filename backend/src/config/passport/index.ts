77d2d2b01b6b378f8a382c0b522153665b442e4c
tsegaye27 <ttalegn29@gmail.com>
2025-07-07 09:33:55 +0300

    Remove commented-out googleStrategy import in passport configuration

--- backend/src/config/passport/index.ts
+++ backend/src/config/passport/index.ts
@@ -1,12 +1,10 @@
 import passport from "passport";
 import localStrategy from "./localStrategy";
 import jwtStrategy from "./jwtStrategy";
-// import googleStrategy from "./googleStrategy";
 
 const initiatePassport = () => {
   passport.use(localStrategy);
   passport.use(jwtStrategy);
-  // passport.use(googleStrategy);
 };
 
 export default initiatePassport;
