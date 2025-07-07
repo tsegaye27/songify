b3d2b9c923b94016ae2fcaedf4aa40da37e12edf
tsegaye27 <ttalegn29@gmail.com>
2025-07-07 09:55:59 +0300

    Remove unused methods import from song model

--- backend/src/models/song/index.ts
+++ backend/src/models/song/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import songSchema from "./schema";
 import * as Statics from "./statics";
-import * as Methods from "./methods";
 import { registerHooks } from "./hooks";
 import { SongInterface, SongModelInterface } from "./types";
 
